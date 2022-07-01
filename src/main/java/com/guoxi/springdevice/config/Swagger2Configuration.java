37d7069fcc7b48bb01ebbfb93b6be47808fc5ee4
guoxi <guoxi@tg-hd.com>
2022-07-01 16:26:57 +0800

    swagger2 添加token可以访问接口API

--- src/main/java/com/guoxi/springdevice/config/Swagger2Configuration.java
+++ src/main/java/com/guoxi/springdevice/config/Swagger2Configuration.java
@@ -4,45 +4,54 @@ package com.guoxi.springdevice.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
+import springfox.documentation.service.Parameter;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
- *
  * swagger2配置
  *
- * @author  guoxi@tg-hd.com
+ * @author guoxi@tg-hd.com
  * @since 2022/6/9 10:11
- *
  */
 @EnableSwagger2
 @Configuration
 public class Swagger2Configuration {
-  @Bean
-  public Docket createRestApi() {
-    return new Docket(DocumentationType.SWAGGER_2)
-            .apiInfo(apiInfo())
-            .select()
-            //为当前包路径
-            .apis(RequestHandlerSelectors.basePackage("com.guoxi.springdevice"))
-            .paths(PathSelectors.any())
-            .build();
-  }
-  private ApiInfo apiInfo() {
-    return new ApiInfoBuilder()
-            //页面标题
-            .title("Spring Boot整合Swagger2 构建RESTful API")
-            //创建人
-            .contact(new Contact("guoxi", "http://localhost:8080", "guoxi_789@126.com"))
-            //版本号
-            .version("1.0")
-            //描述
-            .description("API 描述")
-            .build();
-  }
+    @Bean
+    public Docket createRestApi() {
+        ParameterBuilder tokenPar = new ParameterBuilder();
+        List<Parameter> params = new ArrayList<Parameter>();
+        tokenPar.name("Authentication").description("令牌").modelRef(new ModelRef("string")).parameterType("header").required(false).build();
+        params.add(tokenPar.build());
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.guoxi.springdevice.controller"))
+                .paths(PathSelectors.any())
+                .build()
+                .globalOperationParameters(params)
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                //页面标题
+                .title("Spring Boot整合Swagger2 构建RESTful API")
+                //创建人
+                .contact(new Contact("guoxi", "http://localhost:8080", "guoxi_789@126.com"))
+                //版本号
+                .version("1.0")
+                //描述
+                .description("API 描述")
+                .build();
+    }
 }
